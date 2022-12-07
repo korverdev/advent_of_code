6932f646169322c074ff205ef0244e8323718a86
Casey Korver <casey@korver.dev>
2022-12-06 21:34:08 -0600

    Solved day 5 part 1

---
+++ day_5/part_1.py
@@ -0,0 +1,47 @@
+MAX_CRATE_STACKS = 9
+
+
+def clean_crate_input(str_: str) -> str:
+    return str_.strip("[] ")
+
+
+crate_stacks = [[] for _ in range(MAX_CRATE_STACKS)]
+
+
+try:
+    # Fill initial crate stacks.
+    while True:
+        line = input()
+
+        if line.strip():
+            # If line lists crates.
+            if line.strip()[0] == "[":
+                space_between_crates = 4
+                crates = [clean_crate_input(line[index:index+space_between_crates]) for index in range(0, len(line), space_between_crates)]
+
+                for index, crate in enumerate(crates):
+                    if crate:
+                        crate_stacks[index].insert(0, crate)
+
+        else:
+            break
+    
+    while True:
+        words = input().split()
+
+        amount_of_crates = int(words[1])
+        from_stack = int(words[3]) - 1
+        to_stack = int(words[5]) - 1
+
+        for _ in range(amount_of_crates):
+            crate = crate_stacks[from_stack].pop()
+            crate_stacks[to_stack].append(crate)
+
+except KeyboardInterrupt:
+    message = []
+
+    for stack in crate_stacks:
+        if stack:
+            message.append(stack.pop())
+    
+    print("".join(message))
